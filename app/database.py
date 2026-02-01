c20118480daee66fdaade6e6832da99d1a165141
Samir <mksamir013@gmail.com>
2026-02-01 15:37:26 +0530

    update

--- app/database.py
+++ app/database.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
